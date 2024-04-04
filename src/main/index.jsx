39da06866fe0026a075a2fed343184a687433998
naraeng <nauurr@naver.com>
2024-04-04 14:05:15 +0900

    Bug: resolve merge error

--- src/main/index.jsx
+++ src/main/index.jsx
@@ -1,3 +1,10 @@
+import Header from "../layout/Header";
+import MainBanner from "./_component/MainBanner";
+import MainNotice from "./_component/MainNotice";
+import MainLank from "./_component/MainLank";
+import Footer from "../layout/Footer";
+import "./mainPage.css";
+
 export default function MainPage() {
     return (
         <>
@@ -16,6 +23,7 @@ export default function MainPage() {
                 <MainNotice />
                 <MainLank />
             </div>
+            <Footer />
         </>
     )
 }
