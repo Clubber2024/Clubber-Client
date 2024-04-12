b61e295e74d225d04a269229ff5a22168ba651a8
Kangyeeun0 <kangyeeun55@gmail.com>
2024-04-13 01:22:16 +0900

    Feat:bookmark&login_UI (#10)

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,19 @@
-<<<<<<< HEAD
+import Header from './layout/Header';
 import MainPage from './main';
-import BookMarkPage from './menu/bookmark';
-import LoginPage from './menu/login';
+import CentralClubPage from './menu/central_club';
+import Footer from './layout/Footer';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
     return (
-        <div>
-            <BookMarkPage />
-        </div>
+        <BrowserRouter>
+            <Header />
+            <Routes>
+                <Route path="/main" element={<MainPage />} />
+                <Route path="/menu/central_club" element={<CentralClubPage />} />
+            </Routes>
+            <Footer />
+        </BrowserRouter>
     );
-=======
-import Header from "./layout/Header";
-import MainPage from "./main";
-import CentralClubPage from "./menu/central_club";
-import Footer from "./layout/Footer";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-function App() {
-  return (
-    <BrowserRouter>
-      <Header />
-      <Routes>
-      <Route path='/main' element={<MainPage />} />
-        <Route path='/menu/central_club' element={<CentralClubPage />} />
-      </Routes>
-      <Footer />
-    </BrowserRouter>
-  );
->>>>>>> ff0568d28901e9b8d0c9bac3ed830d41117fb38e
 }
 export default App;
