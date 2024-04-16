f53f2df0d3b63926de5fc3b6c25114a4db4d4519
Kangyeeun0 <kangyeeun55@gmail.com>
2024-04-16 14:19:46 +0900

    Feat : review write page (#15)

---
+++ src/menu/review/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReviewWrite from './component/reviewWrite';
+
+export default function ReviewPage() {
+    return (
+        <>
+            <ReviewWrite />
+        </>
+    );
+}
