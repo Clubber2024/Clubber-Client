88b341c6c9863589660e73de69617eed2faa8a56
Kangyeeun0 <kangyeeun55@gmail.com>
2024-07-22 23:01:37 +0900

    fix: 관리자페이지 버튼 css 수정

--- src/config/axios-config.jsx
+++ src/config/axios-config.jsx
@@ -1,32 +1,32 @@
-import axios from "axios";
+import axios from 'axios';
 
 //export const URL = "http://localhost:8080";
-export const URL = "http://13.125.141.171:8080";
+export const URL = 'http://13.125.141.171:8080';
 // axios.defaults.withCredentials = true;
 
 export const customAxios = axios.create({
-  //baseURL: URL,
-  withCredentials: true, // withCredentials를 true로 설정하여 CORS 요청 시 쿠키를 포함합니다
+    baseURL: URL,
+    withCredentials: true, // withCredentials를 true로 설정하여 CORS 요청 시 쿠키를 포함합니다
 });
 
 customAxios.interceptors.request.use(
-  //axios 요청 직전에 발생하는 함수
-  function (config) {
-    return config;
-  },
-  //axios 요청 에러나면 발생하는 함수
-  function (error) {
-    return Promise.reject(error);
-  }
+    //axios 요청 직전에 발생하는 함수
+    function (config) {
+        return config;
+    },
+    //axios 요청 에러나면 발생하는 함수
+    function (error) {
+        return Promise.reject(error);
+    }
 );
 customAxios.interceptors.response.use(
-  //axios 올바른 response가오면 발동하는 함수
-  function (response) {
-    return response;
-  },
+    //axios 올바른 response가오면 발동하는 함수
+    function (response) {
+        return response;
+    },
 
-  //에러 메시지가 response되면 발동되는 함수
-  async function (error) {
-    return Promise.reject(error);
-  }
+    //에러 메시지가 response되면 발동되는 함수
+    async function (error) {
+        return Promise.reject(error);
+    }
 );
