948d1cd5f397524b2cf22e92c2fd3054ed294430
Kangyeeun0 <kangyeeun55@gmail.com>
2024-07-29 16:57:14 +0900

    feat:버튼 css 수정 및 action 설정

--- src/component/admin/component/SideBar.jsx
+++ src/component/admin/component/SideBar.jsx
@@ -1,51 +1,45 @@
-import styles from "./sideBar.module.css";
-import MouseImg from "../mouse.png";
-import styled from "styled-components";
-import { LinkItem } from "../../branch/BranchCentral";
-import { NavLink } from "react-router-dom";
-import { Nav } from "react-bootstrap";
+import styles from './sideBar.module.css';
+import MouseImg from '../mouse.png';
+import styled from 'styled-components';
+import { NavLink } from 'react-router-dom';
 
 const Img = styled.img`
-  width: 120px;
-  height: 120px;
-  display: block;
-  margin: auto;
-  margin-top: 10%;
+    width: 120px;
+    height: 120px;
+    display: block;
+    margin: auto;
+    margin-top: 10%;
 `;
 
 export default function SideBar() {
-  return (
-    <>
-      <div>
-        <div className={styles.rectangleSide}>
-          <Img src={MouseImg} />
-          <p className={styles.adminText}>관리자</p>
-        </div>
+    return (
+        <>
+            <div>
+                <div className={styles.rectangleSide}>
+                    <Img src={MouseImg} />
+                    <p className={styles.adminText}>관리자</p>
+                </div>
 
-        <div className={styles.rectangleList}>
-          <menu className={styles.sideTitle}>내 정보</menu>
-          <NavLink
-            to={`/admin/edit`}
-            className={({ isActive }) =>
-              isActive ? styles.activeMenu : styles.sideText
-            }
-          >
-            동아리글 작성하기
-          </NavLink>
-          <div className={styles.sideLine}></div>
-          <menu className={styles.sideTitle}>리뷰</menu>
-          <menu className={styles.sideText}>리뷰목록</menu>
+                <div className={styles.rectangleList}>
+                    <menu className={styles.sideTitle}>내 정보</menu>
+                    <NavLink
+                        to={`/admin/edit`}
+                        className={({ isActive }) => (isActive ? styles.activeMenu : styles.sideTextWrite)}
+                    >
+                        동아리글 작성하기
+                    </NavLink>
+                    <div className={styles.sideLine}></div>
+                    <menu className={styles.sideTitle}>리뷰</menu>
+                    <menu className={styles.sideText}>리뷰목록</menu>
 
-          <NavLink
-            to={`/admin/mypage/pending`}
-            className={({ isActive }) =>
-              isActive ? styles.activeMenu : styles.sideText
-            }
-          >
-            리뷰승인대기
-          </NavLink>
-        </div>
-      </div>
-    </>
-  );
+                    <NavLink
+                        to={`/admin/mypage/pending`}
+                        className={({ isActive }) => (isActive ? styles.activeMenu : styles.sideText)}
+                    >
+                        리뷰승인대기
+                    </NavLink>
+                </div>
+            </div>
+        </>
+    );
 }
