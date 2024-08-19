d31c68c6aebdd9185911e54d46c9e67aff1fdaec
naraeng <nauurr@naver.com>
2024-08-19 23:13:16 +0900

    Feat : notice detail page (#77)

--- src/component/main/NoticeList.jsx
+++ src/component/main/NoticeList.jsx
@@ -48,7 +48,7 @@ export default function NoticeList() {
       <div className="notice_list">
         <div className="notice_header">
           <span>번호</span>
-          <span className="notice_title">제목</span>
+          <span style={{textAlign: "start"}}>제목</span>
           <span>날짜</span>
           <span>작성자</span>
           <span>조회수</span>
