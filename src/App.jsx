8fafad571c3acc992bedbaf7b430492372c353d8
Kangyeeun0 <kangyeeun55@gmail.com>
2025-02-07 16:22:10 +0900

    fix: footer

--- src/App.jsx
+++ src/App.jsx
@@ -32,13 +32,21 @@ import ModifyPassword from './component/mypage/ModifyPassword';
 import AdminRecruitWrite from './component/admin/recruit/AdminRecruitWrite';
 import DetailRecruitList from './component/recruit/DetailRecruitList';
 import AdminRecruitContent from './component/admin/recruit/AdminRecruitContent';
+import { minHeight } from '@mui/system';
+import { Padding } from '@mui/icons-material';
 
 function App() {
     const appStyle = {
         backgroundColor: '#f9f9f9',
         margin: 0,
         padding: 0,
-      };
+    };
+
+    const wrapper = {
+        height: 'auto',
+        minHeight: '100%',
+        paddingBottom: '110px',
+    };
 
     const isPc = useMediaQuery({
         query: '(min-width:769px)',
@@ -51,86 +59,89 @@ function App() {
         <div style={appStyle}>
             {isPc && (
                 <BrowserRouter>
-                    <Header />
-                    <Routes>
-                        <Route path="/" element={<MainPage />} />
-                        <Route path="/notices" element={<NoticeList />} />
-                        <Route path="/notices/:noticeId" element={<NoticePage />} />
-                        <Route path="/central" element={<CentralClubPage />} />
-                        {/* <Route path="/menu/central_club/detail_page/review_write" element={<ReviewWrite />} /> */}
-                        <Route path="/small" element={<SmallClubPage />} />
-                        <Route path="/clubs/:clubId" element={<DetailPage />} />
-                        <Route path="/clubs/:clubId/review" element={<ReviewWrite />} />
-                        <Route path="/clubs/:clubId/review/comment" element={<ReviewComment />} />
-                        <Route path="/central/divisions" element={<BranchCentralPage />} />
-                        <Route path="/login" element={<LoginPage />} />
-                        <Route path="/v1/auths/oauth/kakao" element={<KakaoRedirection />} />
-                        <Route path="/small/colleges" element={<BranchSmallPage />} />
-                        <Route path="/hashtag" element={<HashTagPage />} />
-                        <Route path="/search" element={<SearchPage />} />
-                        <Route path="/summary" element={<SummaryPage />} />
-                        <Route path="/user" element={<MyPage />} />
-                        <Route path="/user/bookmark" element={<BookMarkPage />} />
-                        <Route path="/user/reviews" element={<MyReview />} />
-                        <Route path="/recruit" element={<RecruitPage />} />
-                        <Route path="/recruit/:recruitId" element={<RecruitContent />} />
-                        <Route path="/recruit/club/:clubId" element={<DetailRecruitList />} />
+                    <div style={wrapper}>
+                        <Header />
+                        <Routes>
+                            <Route path="/" element={<MainPage />} />
+                            <Route path="/notices" element={<NoticeList />} />
+                            <Route path="/notices/:noticeId" element={<NoticePage />} />
+                            <Route path="/central" element={<CentralClubPage />} />
+                            {/* <Route path="/menu/central_club/detail_page/review_write" element={<ReviewWrite />} /> */}
+                            <Route path="/small" element={<SmallClubPage />} />
+                            <Route path="/clubs/:clubId" element={<DetailPage />} />
+                            <Route path="/clubs/:clubId/review" element={<ReviewWrite />} />
+                            <Route path="/clubs/:clubId/review/comment" element={<ReviewComment />} />
+                            <Route path="/central/divisions" element={<BranchCentralPage />} />
+                            <Route path="/login" element={<LoginPage />} />
+                            <Route path="/v1/auths/oauth/kakao" element={<KakaoRedirection />} />
+                            <Route path="/small/colleges" element={<BranchSmallPage />} />
+                            <Route path="/hashtag" element={<HashTagPage />} />
+                            <Route path="/search" element={<SearchPage />} />
+                            <Route path="/summary" element={<SummaryPage />} />
+                            <Route path="/user" element={<MyPage />} />
+                            <Route path="/user/bookmark" element={<BookMarkPage />} />
+                            <Route path="/user/reviews" element={<MyReview />} />
+                            <Route path="/recruit" element={<RecruitPage />} />
+                            <Route path="/recruit/:recruitId" element={<RecruitContent />} />
+                            <Route path="/recruit/club/:clubId" element={<DetailRecruitList />} />
 
-                        <Route path="/qna" element={<QnAPage />} />
+                            <Route path="/qna" element={<QnAPage />} />
 
-                        <Route path="/admin" element={<MyPage />} />
-                        <Route path="/admin/mypage" element={<AdminPage />} />
-                        <Route path="/admin/edit/:clubId" element={<EditPage />} />
-                        <Route path="/admin/mypage/reviews" element={<ClubReviews />} />
-                        <Route path="/admin/mypage/pending" element={<PendingList />} />
-                        <Route path="/admin/recruit" element={<AdminRecruitList />} />
-                        <Route path="/admin/recruit/edit" element={<AdminRecruitWrite />} />
-                        <Route path="/admin/recruit/:recruitId" element={<AdminRecruitContent />} />
-                        <Route path="/admin/password" element={<ModifyPassword />} />
-                    </Routes>
+                            <Route path="/admin" element={<MyPage />} />
+                            <Route path="/admin/mypage" element={<AdminPage />} />
+                            <Route path="/admin/edit/:clubId" element={<EditPage />} />
+                            <Route path="/admin/mypage/reviews" element={<ClubReviews />} />
+                            <Route path="/admin/mypage/pending" element={<PendingList />} />
+                            <Route path="/admin/recruit" element={<AdminRecruitList />} />
+                            <Route path="/admin/recruit/edit" element={<AdminRecruitWrite />} />
+                            <Route path="/admin/recruit/:recruitId" element={<AdminRecruitContent />} />
+                            <Route path="/admin/password" element={<ModifyPassword />} />
+                        </Routes>
+                    </div>
                     <Footer />
                 </BrowserRouter>
             )}
 
             {isMobile && (
                 <BrowserRouter>
-                    <Header />
-                    <Routes>
-                        <Route path="/" element={<MainPage />} />
-                        <Route path="/notices" element={<NoticeList />} />
-                        <Route path="/notices/:noticeId" element={<NoticePage />} />
-                        <Route path="/central" element={<CentralClubPage />} />
-                        <Route path="/small" element={<SmallClubPage />} />
-                        <Route path="/clubs/:clubId" element={<DetailPage />} />
-                        <Route path="/clubs/:clubId/review" element={<ReviewWrite />} />
-                        <Route path="/clubs/:clubId/review/comment" element={<ReviewComment />} />
-                        <Route path="/central/divisions" element={<BranchCentralPage />} />
-                        <Route path="/login" element={<LoginPage />} />
-                        <Route path="/v1/auths/oauth/kakao" element={<KakaoRedirection />} />
-                        <Route path="/small/colleges" element={<BranchSmallPage />} />
-                        <Route path="/hashtag" element={<HashTagPage />} />
-                        <Route path="/search" element={<SearchPage />} />
-                        <Route path="/summary" element={<SummaryPage />} />
-                        <Route path="/user" element={<MyPage />} />
-                        <Route path="/user/bookmark" element={<BookMarkPage />} />
-                        <Route path="/user/reviews" element={<MyReview />} />
-                        <Route path="/qna" element={<QnAPage />} />
-                        <Route path="/recruit" element={<RecruitPage />} />
-                        <Route path="/recruit/:recruitId" element={<RecruitContent />} />
-                        <Route path="/recruit/club/:clubId" element={<DetailRecruitList />} />
+                    <div style={wrapper}>
+                        <Header />
+                        <Routes>
+                            <Route path="/" element={<MainPage />} />
+                            <Route path="/notices" element={<NoticeList />} />
+                            <Route path="/notices/:noticeId" element={<NoticePage />} />
+                            <Route path="/central" element={<CentralClubPage />} />
+                            <Route path="/small" element={<SmallClubPage />} />
+                            <Route path="/clubs/:clubId" element={<DetailPage />} />
+                            <Route path="/clubs/:clubId/review" element={<ReviewWrite />} />
+                            <Route path="/clubs/:clubId/review/comment" element={<ReviewComment />} />
+                            <Route path="/central/divisions" element={<BranchCentralPage />} />
+                            <Route path="/login" element={<LoginPage />} />
+                            <Route path="/v1/auths/oauth/kakao" element={<KakaoRedirection />} />
+                            <Route path="/small/colleges" element={<BranchSmallPage />} />
+                            <Route path="/hashtag" element={<HashTagPage />} />
+                            <Route path="/search" element={<SearchPage />} />
+                            <Route path="/summary" element={<SummaryPage />} />
+                            <Route path="/user" element={<MyPage />} />
+                            <Route path="/user/bookmark" element={<BookMarkPage />} />
+                            <Route path="/user/reviews" element={<MyReview />} />
+                            <Route path="/qna" element={<QnAPage />} />
+                            <Route path="/recruit" element={<RecruitPage />} />
+                            <Route path="/recruit/:recruitId" element={<RecruitContent />} />
+                            <Route path="/recruit/club/:clubId" element={<DetailRecruitList />} />
 
-                        <Route path="/admin" element={<MyPage />} />
-                        <Route path="/admin/mypage" element={<AdminPage />} />
-                        <Route path="/admin/edit/:clubId" element={<EditPage />} />
-                        <Route path="/admin/mypage/reviews" element={<ClubReviews />} />
-                        <Route path="/admin/mypage/pending" element={<PendingList />} />
+                            <Route path="/admin" element={<MyPage />} />
+                            <Route path="/admin/mypage" element={<AdminPage />} />
+                            <Route path="/admin/edit/:clubId" element={<EditPage />} />
+                            <Route path="/admin/mypage/reviews" element={<ClubReviews />} />
+                            <Route path="/admin/mypage/pending" element={<PendingList />} />
 
-                        <Route path="/admin/recruit" element={<AdminRecruitList />} />
-                        <Route path="/admin/recruit/edit" element={<AdminRecruitWrite />} />
-                        <Route path="/admin/recruit/:recruitId" element={<AdminRecruitContent />} />
-                        <Route path="/admin/password" element={<ModifyPassword />} />
-                    </Routes>
-
+                            <Route path="/admin/recruit" element={<AdminRecruitList />} />
+                            <Route path="/admin/recruit/edit" element={<AdminRecruitWrite />} />
+                            <Route path="/admin/recruit/:recruitId" element={<AdminRecruitContent />} />
+                            <Route path="/admin/password" element={<ModifyPassword />} />
+                        </Routes>
+                    </div>
                     <Footer />
                 </BrowserRouter>
             )}
