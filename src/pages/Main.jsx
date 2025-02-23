6d0ecc53ac09b85eac870221c259df796fdf09cd
naraeng <108282052+naraeng@users.noreply.github.com>
2025-02-23 15:56:50 +0900

    Revert "Refactor: routing calendar (#210)"

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,7 +1,7 @@
 import MainBanner from '../component/main/MainBanner';
 import MainRanking from '../component/main/MainRanking';
 import MainNotice from '../component/main/MainNotice';
-// import MainPromote from '../component/main/MainPromote';
+import MainPromote from '../component/main/MainPromote';
 import './mainPage.css';
 import QnAMain from '../component/QnA/QnAMain';
 import MainIco from '../component/main/MainIco';
